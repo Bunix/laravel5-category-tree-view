cd2d9b5efbbcb2f964a9d8126de15d913671600e
marcovaccarini <quirkmode@yahoo.com>
2016-11-21 23:57:22 +0100

    correct typo error and Form dependencies and singleton

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,35 +1,73 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
-use App\Http\Request;
+
+use App\Http\Requests;
+
 use App\Category;
 
-Class CategoryController extends Controller {
+
+class CategoryController extends Controller
+
+{
+
     /**
+
      * Show the application dashboard.
+
      *
+
      * @return \Illuminate\Http\Response
+
      */
+
     public function manageCategory()
+
     {
-        $categories = Category::where('parent_di', '=', 0)->get();
-        $allcategories = Category::pluck('title', 'id')->all();
-        return view('CategoryTreeview', compact('categories', 'allCategories'));
+
+        $categories = Category::where('parent_id', '=', 0)->get();
+
+        $allCategories = Category::pluck('title','id')->all();
+
+        return view('categoryTreeview',compact('categories','allCategories'));
+
     }
 
+
+    /**
+
+     * Show the application dashboard.
+
+     *
+
+     * @return \Illuminate\Http\Response
+
+     */
+
     public function addCategory(Request $request)
+
     {
+
         $this->validate($request, [
-                        'title' => 'required'
+
+            'title' => 'required',
+
         ]);
+
         $input = $request->all();
-        $input['parent_id'] = empty($input[parent_id]) ? 0 : $input['parent_id'];
+
+        $input['parent_id'] = empty($input['parent_id']) ? 0 : $input['parent_id'];
+
+
 
         Category::create($input);
-        return back()->with('success', 'New category added successfully!')
-        
+
+        return back()->with('success', 'New Category added successfully.');
+
     }
 
 
